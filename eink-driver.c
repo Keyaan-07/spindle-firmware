74f351a69c8ba164d5f52315b4193d4daa3ea680
Keyaan <keyaan.1911@gmail.com>
2026-09-22 23:24:33 +0530

    initialisation finally complete

--- eink-driver.c
+++ eink-driver.c
@@ -13,6 +13,8 @@
 #define PIN_RSTN 6
 #define PIN_BUSY 7 // if BUSY = 1, pause sending commands, as the device is BUSY; if BUSY = 0, no interruption needed
 
+// for all arrays, the first value is the command, and the following value(s) are the data values. 
+
 
 int eink_init(){
 
@@ -57,10 +59,65 @@ int eink_init(){
         sleep_ms(1);
     }
 
+
     // 10ms sleep
     sleep_ms(10);
 
 
-    
-    
+    // setting gate driver output
+    uint8_t gate_driver_config[] = {0x01, 0x27, 0x01, 0x00};
+    gpio_put(PIN_DC,0);
+    gpio_put(PIN_CS, 0);
+    spi_write_blocking(SPI_PORT, &gate_driver_config[0], 1);
+    gpio_put(PIN_DC, 1);
+    spi_write_blocking(SPI_PORT, &gate_driver_config[1], 3);
+    gpio_put(PIN_CS, 1);
+    sleep_ms(1);
+
+
+    // setting display RAM size
+    uint8_t data_entry_mode_setting[] = {0x11, 0x03};
+    gpio_put(PIN_DC, 0);
+    gpio_put(PIN_CS, 0);
+    spi_write_blocking(SPI_PORT, &data_entry_mode_setting[0], 1);
+    gpio_put(PIN_DC, 1);
+    spi_write_blocking(SPI_PORT, &data_entry_mode_setting[1], 1);
+    gpio_put(PIN_CS, 1);
+    sleep_ms(1);
+
+
+    // setting RAM X address start and end position
+    uint8_t ram_x_start_end[] = {0x44, 0x00, 0x15};
+    gpio_put(PIN_DC, 0);
+    gpio_put(PIN_CS, 0);
+    spi_write_blocking(SPI_PORT, &ram_x_start_end[0], 1);
+    gpio_put(PIN_DC, 1);
+    spi_write_blocking(SPI_PORT, &ram_x_start_end[1], 2);
+    gpio_put(PIN_CS, 1);
+    sleep_ms(1);
+
+
+    // setting RAM Y address start and end position
+    uint8_t ram_y_start_end[] = {0x45, 0x00, 0x00, 0x27, 0x01};
+    gpio_put(PIN_DC, 0);
+    gpio_put(PIN_CS, 0);
+    spi_write_blocking(SPI_PORT, &ram_y_start_end[0], 1);
+    gpio_put(PIN_DC, 1);
+    spi_write_blocking(SPI_PORT, &ram_y_start_end[1], 4);
+    gpio_put(PIN_CS, 1);
+    sleep_ms(1);
+
+
+    // border waveform control
+    uint8_t border_waveform_control[] = {0x3C, 0xC0};
+    gpio_put(PIN_DC, 0);
+    gpio_put(PIN_CS, 0);
+    spi_write_blocking(SPI_PORT, &border_waveform_control[0], 1);
+    gpio_put(PIN_DC, 1);
+    spi_write_blocking(SPI_PORT, &border_waveform_control[1], 1);
+    gpio_put(PIN_CS, 1);
+    sleep_ms(1);
+
+
+    //initialisation complete. 
 }
